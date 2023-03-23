c39f50908b8b4b7002d17097e2cdd19e4017485f
priti27599 <75195316+priti27599@users.noreply.github.com>
2023-03-23 17:29:01 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+  const skill = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React.js',
+    'Java',
+    'Next.js',
+    'Java',
+  ];
   return (
     <div className="main">
-      <h1>SUBSCRIBE</h1>
-      <p>Sign up with your email address to receive news and update. </p>
-      <div className="inputbox">
-        <input placeholder="First name" />
-        <input placeholder="Last name" />
-        <input placeholder="Email" />
+      <img
+        src="https://tse1.mm.bing.net/th?id=OIP.y-nGyqT5AwES8oqp344z4gHaHa&pid=Api&rs=1&c=1&qlt=95&w=113&h=113"
+        alt="profile"
+      />
+
+      <h3>Priti Kumari</h3>
+      <p>Frontend Developer</p>
+
+      <div>
+        <h2>Skills</h2>
+        <ul>
+          {skill.map((sk) => (
+            <li key={sk}>{sk}</li>
+          ))}
+        </ul>
       </div>
-      <button>Subscribe</button>
+      <p>Joined on 06 dec 2021</p>
     </div>
   );
 }
